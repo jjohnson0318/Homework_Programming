a50c5b5d6fa050870fb5d5540053e2ba8e738433
jjohnson0318 <josejohnson0318@gmail.com>
2025-02-22 14:06:20 -0500

    Have to finish other 30%

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,5 +2,57 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    public GameObject preFab;
+    public Transform bulletTrash;
+    public Transform bulletSpam;
 
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
+
+    private void Update()
+    {
+        if (!_canShoot)
+        {
+            _canShoot = true;
+            _currentTime = Timer;
+        }
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject bullet = Instantiate(preFab, bulletSpam.position, Quaternion.identity);
+
+            bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+        }
+    }
+
+    private void TimerMethod()
+    {
+        TimerMethod();
+        Shoot();
+        if (!_canShoot)
+        {
+            _currentTime -= Time.deltaTime;
+        }
+        if (_currentTime < 0)
+        {
+            _canShoot = true;
+            _currentTime = Timer;
+        }
+    }
+
+    private void Shoot()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        {
+            GameObject bullet = Instantiate(preFab, bulletSpam.position, Quaternion.identity);
+            bullet.transform.SetParent(bulletTrash);
+            _canShoot = false;
+        }
+    }
 }
